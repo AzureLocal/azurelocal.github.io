3f4a17a443bebe6a73ced42220fb1ff8933a672f
Kris Turner <kristopherjturner@outlook.com>
2025-03-31 16:45:29 -0400

    version update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -83,7 +83,7 @@ const config = {
         items: [
           {
             type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
+            sidebarId: 'sidebar',
             position: 'left',
             label: 'Docs',
           },
