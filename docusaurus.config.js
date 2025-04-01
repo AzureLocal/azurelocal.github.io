a75791ed93635505df3467af120569536feac631
Kris Turner <kristopherjturner@outlook.com>
2025-03-31 21:24:36 -0400

    Social Card Update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,7 +73,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/azure-local-social-card.jpg',
       navbar: {
         title: 'Azure Local',
         logo: {
